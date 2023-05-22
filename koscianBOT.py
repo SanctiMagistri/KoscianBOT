ee49827ab75720562fc8c523fd7076fb784458ac
SanctiMagistri <wasylukmateusz@gmail.com>
2023-05-22 16:49:33 +0200

    Added responding message.

--- koscianBOT.py
+++ koscianBOT.py
@@ -3,6 +3,9 @@ import os
 import discord
 from dotenv import load_dotenv
 
+import responses
+
+
 def run_bot():
     load_dotenv()
     TOKEN = os.environ['DISCORD_TOKEN']
@@ -13,6 +16,29 @@ def run_bot():
     @client.event
     async def on_ready():
         print(f'{client.user} Podłączono')
+
+    async def send_message(message, user_message, is_private):
+        try:
+            response = responses.handle_response(user_message)
+            await message.author.send(response) if is_private else await message.channel.send(response)
+        except Exception as e:
+            print(e)
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f"{username} said: '{user_message}' ({channel})")
+
+        if user_message[0] == '?':
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
 
     client.run(TOKEN)
 
---
+++ responses.py
@@ -0,0 +1,11 @@
+
+def handle_response(message) -> str:
+    p_message = message.lower()
+    print(p_message)
+
+    if p_message == "hello there":
+        return "General Kenobi!"
+
+
+
+    return "Co xD"
